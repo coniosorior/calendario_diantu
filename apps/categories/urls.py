7c9d071943b2e21ec877136823ac5a374b1c5145
Coniosorior <cosorio.riquelme@gmail.com>
2026-08-08 01:34:06 -0400

    refactor(categories): eliminar rutas de crear editar y eliminar categoria

--- apps/categories/urls.py
+++ apps/categories/urls.py
@@ -5,7 +5,4 @@ app_name = 'categories'
 
 urlpatterns = [
     path('', views.CategoryListView.as_view(), name='list'),
-    path('nueva/', views.CategoryCreateView.as_view(), name='create'),
-    path('<int:pk>/editar/', views.CategoryUpdateView.as_view(), name='update'),
-    path('<int:pk>/eliminar/', views.CategoryDeleteView.as_view(), name='delete'),
 ]
