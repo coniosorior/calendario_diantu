5154305679a5e1fbed6278af584179ce44f97ff1
Coniosorior <cosorio.riquelme@gmail.com>
2026-08-25 18:48:52 -0400

    feature(accounts): agregar loginform para estilizar campos de login

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
 
@@ -39,3 +39,20 @@ class RegistroForm(UserCreationForm):
         self.fields['username'].help_text = None
         self.fields['password1'].help_text = None
         self.fields['password2'].help_text = None
+
+
+class LoginForm(AuthenticationForm):
+    """
+    Extiende AuthenticationForm de Django solo para agregar las clases CSS
+    a los campos username/password, ya que el formulario nativo no las trae.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({
+            'class': 'form-input',
+            'placeholder': 'tu_usuario',
+        })
+        self.fields['password'].widget.attrs.update({
+            'class': 'form-input',
+            'placeholder': 'Tu contraseña',
+        })
