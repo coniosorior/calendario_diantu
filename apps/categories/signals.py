5b0f0983f005e1c7dd58560583947399a65ab59a
Coniosorior <cosorio.riquelme@gmail.com>
2026-08-28 02:06:27 -0400

    fix(categories): corregir color de dormir en categorias predeterminadas

--- apps/categories/signals.py
+++ apps/categories/signals.py
@@ -8,7 +8,7 @@ DEFAULT_CATEGORIES = [
     {'name': 'Trabajo/Estudio', 'color': '#006EE9', 'icon': 'ti-briefcase', 'is_default': False},
     {'name': 'Ejercicio',       'color': '#FB5607', 'icon': 'ti-run',        'is_default': False},
     {'name': 'Salud',           'color': '#8338EC', 'icon': 'ti-stethoscope', 'is_default': False},
-    {'name': 'Dormir',          'color': '#415A77', 'icon': 'ti-moon',        'is_default': False},
+    {'name': 'Dormir',          'color': '#415A76', 'icon': 'ti-moon',        'is_default': False},
     {'name': 'Comida',          'color': '#8BC34A', 'icon': 'ti-tools-kitchen-2', 'is_default': False},
     {'name': 'Descanso',        'color': '#FFBC42', 'icon': 'ti-coffee',      'is_default': False},
     {'name': 'Personal',        'color': '#EA638C', 'icon': 'ti-heart',       'is_default': False},
